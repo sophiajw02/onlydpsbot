e822daaaad81ccf42f1aa230fa6cfb943c1ee485
ahroria <swu331@gmail.com>
2021-02-09 14:33:42 -0500

    edits

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -1,31 +1,86 @@
-from discord.ext.commands import Bot as BotBase
-from discord.ext.commands import CommandNotFound, command
-from discord import Embed, File
 from datetime import datetime
+from glob import glob
+from asyncio import sleep
+
+from discord.ext.commands import Bot as BotBase
+from discord.ext.commands import (CommandNotFound, BadArgument, MissingRequiredArgument)
+from discord.ext.commands import Context
+from discord.errors import HTTPException, Forbidden
+from discord import Embed, File
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.cron import CronTrigger
+
+from ..db import db
 
 PREFIX = "!"
 OWNER_IDS = [524668365349060610]
+COGS = [path.split("\\")[-1][:-3] for path in glob("./lib/cogs/*.py")]
+IGNORE_EXCEPTIONS = (CommandNotFound, BadArgument, MissingRequiredArgument)
+
+class Ready(object):
+    def __init__(self):
+        for cog in COGS:
+            setattr(self, cog, False)
+
+    def ready_up(self, cog):
+        setarr(self, cog, True)
+        print(f" {cog} cog ready")
+
+    def all_ready(self):
+        return all([getattr(self,cog) for cog in COGS])
 
 class Bot(BotBase):
     def __init__(self):
         self.PREFIX = PREFIX
         self.ready = False
+        self.cogs_ready = Ready()
+
         self.guild = None
         self.scheduler = AsyncIOScheduler()
 
+        db.autosave(self.scheduler)
         super().__init__(
             command_prefix=PREFIX, 
             owner_ids=OWNER_IDS) #intents=Intents.all()
 
+    def setup(self):
+        for cog in COGS:
+            self.load_extension(f"lib.cogs.{cog}")
+            print(f" {cog} cog loaded")
+
+        print("set up complete")
+
     def run(self, version):
         self.VERSION = version
+
+        print("running setup")
+        self.setup()
 
         with open("lib/bot/token.0", "r", encoding="utf-8") as tf:
             self.TOKEN = tf.read()
 
         print("running bot...")
         super().run(self.TOKEN, reconnect=True)
+
+    async def process_commands(self, message):
+        ctx = await self.get_context(message, cls=Context)
+
+        if ctx.command is not None and ctx.guild is not None:
+            if any([isinstance(exc,error) for error in IGNORE_EXCEPTIONS]):
+                pass
+            elif isinstance(exc, BadArgument):
+                pass
+            elif isinstance(exc, MissingRequiredArgument):
+                await ctx.send("one or more arguments missing")
+            elif isinstance(exc.original, HTPPException):
+                await ctx.send("unble to send message")
+            elif isinstance(exc.original, Forbidden):
+                await ctx.send("no permission to complete command")
+            else:
+                await exc.original
+
+    async def rules_reminder(self):
+        await self.stdout.send("Please follow the rules!")
 
     async def on_command():
         print("Bot connected and online!")
@@ -36,49 +91,48 @@ class Bot(BotBase):
     async def on_error(self, err, *args, **kwargs):
         if err == "on_command_error":
             await args[0].send("Incorrect command")
-        channel = self.get_channel(805839915463213107)
-        await channel.send("Error")
+        await self.stdout.send("Error")
         raise
 
     async def on_command_error(self, ctx, exc):
         if isinstance(exc, CommandNotFound):
             pass
-        elif hasattr(exc, "original"):
-            raise exc.original
         else:
             raise exc
 
     #embed message on bot start up
     async def on_ready(self):
         if not self.ready:
-            self.ready = True
             self.guild = self.get_guild(758769744362340373)
-            print("bot ready")
-
-            channel = self.get_channel(805839915463213107)
-            #await channel.send("Healbot online and ready!")
-
-            embed = Embed(title="Healbot Online", description="Succor is ready to nyoom", colour=0x16C77D, timestamp=datetime.utcnow())
-            fields = [("Name", "Value", True),
-                      ("Another", "Wow Next", True),
-                      ("A final one", "Last", False)]
-            for name, value, inline in fields:
-                embed.add_field(name=name, value=value, inline=inline)
+            self.stdout = self.get_channel(805839915463213107)
+            self.scheduler.add_job(self.rules_reminder, CronTrigger(day_of_week=0, hour=12, minute=0, second=0))
+            self.scheduler.start()
+           
+            #embed = Embed(title="Healbot Online", description="Succor is ready to nyoom", colour=0x16C77D, timestamp=datetime.utcnow())
+            #fields = [("Name", "Value", True),
+            #          ("Another", "Wow Next", True),
+            #          ("A final one", "Last", False)]
+            #for name, value, inline in fields:
+            #    embed.add_field(name=name, value=value, inline=inline)
             #embed.set_author(name="Soap", icon_url=self.guild.icon_url)
             #embed.set_footer(text="This is a foooooooooterrrr")
-            await channel.send(embed=embed)
-
+            #await channel.send(embed=embed)
             #await channel.send(file=File("data/images/glare.jpg")) #bot sends files in channel
+
+            while not self.cogs_ready.all_ready():
+                await sleep(0.5)
+            
+            await self.stdout.send("Healbot online and ready!")
+            self.ready = True
+            print("bot ready")
+
 
         else:
             print("bot reconnected")
 
     async def on_message(self,message):
-        pass
-
-    self.command()
-    async def test(ctx, arg):
-        await send(arg)
+        if not message.author.bot: #ignore messages from bots
+            self.process_commands(message)
 
 bot = Bot()
         
---
+++ lib/cogs/fun.py
@@ -0,0 +1,40 @@
+from random import choice, randint
+from typing import Optional
+from discord.ext.commands import Cog, command, BadArgument
+from discord import Member
+
+class Fun(cog):
+    def __init__(self, bot):
+        self.bot=bot
+
+    @command(name="hello", aliases=["hi","welcome"]) #name="insertname", aliases=["cmd","ins","more"], hidden=True
+    async def say_hello(self, ctx):
+        await ctx.send(f"{choice(('Hello', 'Hi', 'Sup'))} {ctx.author.mention}!")
+
+    @command(name="dice", aliases=["roll"])
+    async def roll_dice(self, ctx, die_string: str):
+        dice, value = (int(term) for term in dice_sting.split("d")) #input command would be like 5d5 (roll a 6 die 5 times)
+        if dice <= 25:
+            rolls = [randit(1, value) for i in range(dice)]
+            await ctx.send(" + ".join(str(r) for r in rolls) + f" = {sum(rolls)}")
+        else:
+            await ctx.send("too many dice to be rolled, try a lower number")
+
+    @command(name="hug", aliases=["love","comfort"])
+    async def give_hug(self, ctx, member: Member, *, reason: Optional[str] = "no reason"):
+        #await ctx.message.delete()
+            #delete the message the user sent
+        await ctx.send(f"{ctx.author.display_name} hugs {member.mention} for {reason}!")
+
+    @give_hug.error
+    async def give_hug_error(self,ctx, exc):
+        if isinstance(exc, BadArgument):
+            await ctx.send("that member cannot be found")
+
+    @Cog.listener()
+    async def on_ready(self):
+        if not self.bot.ready:
+            self.bot.cogs_ready.ready_up("fun")
+
+def setup(bot):
+    bot.add_cog(Fun(bot))
