acd4f57c106a5578f2edbb98af2c4684b80c3359
ahroria <swu331@gmail.com>
2021-04-05 15:27:13 -0400

    update: removed personal ids

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -16,7 +16,7 @@ from discord.ext.commands import (CommandNotFound, BadArgument, MissingRequiredA
 from ..db import db
 
 PREFIX = "!"
-OWNER_IDS = [524668365349060610] 
+#OWNER_IDS = [524668365349060610] 
 COGS = [os.path.split(path)[1][:-3] for path in glob("./lib/cogs/*.py")]
 # COGS = [path.split("\\")[-1][:-3] for path in glob("./lib/cogs/*.py")]
 IGNORE_EXCEPTIONS = (CommandNotFound, BadArgument)
@@ -46,6 +46,8 @@ class Bot(BotBase):
         self.scheduler = AsyncIOScheduler()
 
         db.autosave(self.scheduler)
+        with open("lib/bot/owner_id.0", "r", encoding="utf-8") as tf:
+            OWNER_IDS = tf.read()
         super().__init__(command_prefix=get_prefix, owner_ids=OWNER_IDS) #intents=Intents.all()
 
     def setup(self):
@@ -58,7 +60,7 @@ class Bot(BotBase):
         self.VERSION = version
         print("running setup. . .")
         self.setup()
-        with open("lib/bot/token.0", "r", encoding="utf-8") as tf:
+        with open("lib/bot/bot_token.0", "r", encoding="utf-8") as tf:
             self.TOKEN = tf.read()
         print("running bot. . .")
         super().run(self.TOKEN, reconnect=True)
@@ -83,8 +85,6 @@ class Bot(BotBase):
     async def on_error(self, err, *args, **kwargs):
         if err == "on_command_error":
             await args[0].send(":x: Incorrect command")
-        # else:
-        #     await self.stdout.send("Error")
         raise
 
     async def on_command_error(self, ctx, exc):
@@ -104,21 +104,14 @@ class Bot(BotBase):
 
     async def on_ready(self):
         if not self.ready:
-            self.guild = self.get_guild(758769744362340373)
-            self.stdout = self.get_channel(758769744362340376) 
+            with open("lib/bot/channel_id.0", "r", encoding="utf-8") as tf:
+                GUILD = tf.read()
+            with open("lib/bot/guild_id.0", "r", encoding="utf-8") as tf:
+                CHANNEL = tf.read()
+            self.guild = self.get_guild(GUILD)
+            self.stdout = self.get_channel(CHANNEL) 
             self.scheduler.add_job(self.rules_reminder, CronTrigger(day_of_week=0, hour=1, minute=0, second=0))
             self.scheduler.start()
-           
-            # embed = Embed(title="Healbot Online", description="Succor is ready to nyoom", colour=0x16C77D, timestamp=datetime.utcnow())
-            # fields = [("Name", "Value", True),
-            #          ("Another", "Wow Next", True),
-            #          ("A final one", "Last", False)]
-            # for name, value, inline in fields:
-            #    embed.add_field(name=name, value=value, inline=inline)
-            # embed.set_author(name="Only DPS Bot", icon_url=self.guild.icon_url)
-            # embed.set_footer(text="This is a foooooooooterrrr")
-            # await self.stdout.send(embed=embed)
-            #await channel.send(file=File("data/images/glare.jpg")) #bot sends files in channel
 
             while not self.cogs_ready.all_ready():
                 await sleep(0.5)
